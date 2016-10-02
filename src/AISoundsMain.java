86d7f2b8760dfc32ba6c45d516f785788977aa7f
c0606764 <c0606764@tamu.edu>
2016-10-02 08:54:11 -0500

    working class and main

--- src/AISoundsMain.java
+++ src/AISoundsMain.java
@@ -1,4 +1,11 @@
 
-public class AISoundsMain {
 
+public class AISoundsMain {
+	public static void main(String[] args){
+		
+		MidiConverter midi = new MidiConverter();
+		//Phrase phrase = midi.music_import("src/audio/Smbtheme.mid");
+		//midi.music_export(phrase, "src/audio/New.mid");
+		midi.music_play("src/audio/Smbtheme.mid");
+	}
 }
--- src/MidiConverter.java
+++ src/MidiConverter.java
@@ -1,3 +1,4 @@
+
 import jm.JMC;
 import jm.music.data.*;
 import jm.music.tools.*;
@@ -7,33 +8,38 @@ public class MidiConverter {
 	
 
 
-			public Phrase[] music_import(String filename){ // music importer: takes in (midi) file,
+			public Phrase music_import(String filename){ // music importer: takes in (midi) file,
 			//interprets file and exports as matrix for us to use
 				
 				//functions associated with Score class
 				Score theScore = new Score("Imported score"); //make new score from midi file
-				Read.midi(theScore, filename); //read midi file called "inputfle.midi"
+				Read.midi(theScore, filename); //read midi file 
 				Part part = theScore.getPart(0); //gets part in score
 				
 
 				//functions associated with Part class
 				//double partend = part.getEndTime();
-				Phrase[] thePhrase = part.getPhraseArray(); //gets phrase array in part
+				Phrase thePhrase = part.getPhrase(0); //gets phrase in part
 					
 				return thePhrase;
 
 			}
-			public void music_export(Phrase[] thePhrase, String filename){
+			
+			public void music_export(Phrase thePhrase, String filename){
 				Part thePart = new Part(); //create empty part
 				Score theScore = new Score("Exported score"); //make new score
-				thePart.addPhraseList(thePhrase); //add phrases to part
+				thePart.addPhrase(thePhrase); //add phrase to part
 				theScore.addPart(thePart); //add part to score
 				Write.midi(theScore, filename);
 			}
 
-			public void music_play(String filename){ //plays music given a file?
+			public void music_play(String filename){ //plays music given a file
 				Score theScore = new Score("Imported score"); //make new score from midi file
-				Read.midi(theScore, filename); //read midi file called "inputfle.midi"
+				Read.midi(theScore, filename); //read midi file 
+				Part part = theScore.getPart(0); //gets part in score
+				part.setInstrument(79);//79 for ocarina, 3 for ht piano
+				
+				theScore.setTempo(105.0);
 				Play.midi(theScore);
 			}
 }
