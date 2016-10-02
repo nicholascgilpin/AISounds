8e8069755779a0a8e456e49c29e218c210eecfc6
c0606764 <c0606764@tamu.edu>
2016-10-01 19:32:04 -0500

    implimented import and export/play methods
    music_import, music_play

--- src/MidiConverter.java
+++ src/MidiConverter.java
@@ -1,4 +1,32 @@
+import jm.JMC;
+import jm.music.data.*;
+import jm.music.tools.*;
+import jm.util.*;
 
 public class MidiConverter {
+	//class variables
 
+
+			public Phrase[] music_import(String filename){ // music importer: takes in (midi) file,
+			//interprets file and exports as matrix for us to use
+				
+				//functions associated with Score class
+				Score theScore = new Score("Imported score"); //make new score from midi file
+				Read.midi(theScore, filename); //read midi file called "inputfle.midi"
+				Part part = theScore.getPart(0); //gets part in score
+				
+
+				//functions associated with Part class
+				double partend = part.getEndTime();
+				Phrase[] phrase = part.getPhraseArray();
+					
+				return phrase;
+
+			}
+
+			public void music_play(String filename){ //plays music given a file?
+				Score theScore = new Score("Imported score"); //make new score from midi file
+				Read.midi(theScore, filename); //read midi file called "inputfle.midi"
+				Play.midi(theScore);
+			}
 }
